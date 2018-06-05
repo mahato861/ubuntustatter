7c9f208d77498fbc164989176c4f4e4c9027ec2b
prabhat pankaj <prabhatiitbhu@gmail.com>
2018-06-05 07:58:17 +0530

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -16,20 +16,20 @@ export LC_MEASUREMENT="en_US.UTF-8"
 export LC_IDENTIFICATION="en_US.UTF-8"
 export LC_ALL=en_US.UTF-8
 
-echo "LANG=en_US.UTF-8" > '/etc/default/locale'
-echo "LANGUAGE=en_US.UTF-8" >> '/etc/default/locale'
-echo "LC_CTYPE="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_NUMERIC="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_TIME="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_COLLATE="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_MONETARY="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_MESSAGES="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_PAPER="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_NAME="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_ADDRESS="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_TELEPHONE="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_MEASUREMENT="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_IDENTIFICATION="en_US.UTF-8"" >> '/etc/default/locale'
-echo "LC_ALL=en_US.UTF-8" >> '/etc/default/locale'
+sudo sh -c "echo '"LANG=en_US.UTF-8"' > /etc/default/locale"
+sudo sh -c "echo '"LANGUAGE=en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_CTYPE="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_NUMERIC="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_TIME="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_COLLATE="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_MONETARY="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_MESSAGES="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_PAPER="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_NAME="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_ADDRESS="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_TELEPHONE="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_MEASUREMENT="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_IDENTIFICATION="en_US.UTF-8"' >> /etc/default/locale"
+sudo sh -c "echo '"LC_ALL=en_US.UTF-8"' >> /etc/default/locale"
 
-locale-gen en_US.UTF-8 && apt-get update 
+sudo locale-gen en_US.UTF-8 && sudo apt-get update 
