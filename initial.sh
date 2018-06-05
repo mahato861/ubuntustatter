ee96950e7454c5a86fc69df0bafc032e18293795
prabhat pankaj <prabhatiitbhu@gmail.com>
2018-06-05 08:09:56 +0530

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -16,6 +16,7 @@ export LC_MEASUREMENT="en_US.UTF-8"
 export LC_IDENTIFICATION="en_US.UTF-8"
 export LC_ALL=en_US.UTF-8
 
+echo 'updating locale'
 sudo sh -c "echo '"LANG=en_US.UTF-8"' > /etc/default/locale"
 sleep 1
 sudo sh -c "echo '"LANGUAGE=en_US.UTF-8"' >> /etc/default/locale"
